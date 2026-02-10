b724c7fec44da89caf675d7094f6ddf704bc4e30
Sterden <53517039+nattakittl57@users.noreply.github.com>
2026-02-10 16:19:46 +0700

    implement upload widget

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,12 @@ import { useNotificationProvider } from "./components/refine-ui/notification/use
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import { dataProvider } from "./providers/data";
 import Dashboard from "@/pages/dashboard.tsx";
-import {BookIcon, HomeIcon} from "lucide-react";
+import {BookIcon, GraduationCapIcon, HomeIcon} from "lucide-react";
 import {Layout} from "@/components/refine-ui/layout/layout.tsx";
 import SubjectList from "@/pages/subjects/list.tsx";
 import SubjectCreate from "@/pages/subjects/create.tsx";
+import ClassesList from "@/pages/classes/list.tsx";
+import ClassesCreate from "@/pages/classes/create.tsx";
 import.meta.env.VITE_BACKEND_BASE_URL
 
 function App() {
@@ -45,6 +47,12 @@ function App() {
                     list: "/subjects",
                     create: "/subjects/create",
                       meta: { label: "Subjects", icon: <BookIcon/> },
+                  },
+                  {
+                      name: "classes",
+                      list: "/classes",
+                      create: "/classes/create",
+                      meta: { label: "Classes", icon: <GraduationCapIcon/> },
                   }
               ]}
             >
@@ -58,6 +66,12 @@ function App() {
                       <Route path="/subjects">
                           <Route index element={<SubjectList/>} />
                           <Route path="create" element={<SubjectCreate />} />
+
+                      </Route>
+
+                      <Route path="/classes">
+                          <Route index element={<ClassesList />} />
+                          <Route path="create" element={<ClassesCreate />} />
 
                       </Route>
                   </Route>
